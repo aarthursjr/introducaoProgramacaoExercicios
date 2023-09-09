9f32a28c13e948f1a1c9e76ad0c4ed97af9e6e4f
André Arhur <contato.andrearthur@gmail.com>
2023-09-08 21:31:47 -0300

    exercicio 5

--- TEMA 4 - Estrutura de Seleção IF-ELSE/exercicio05.c
+++ TEMA 4 - Estrutura de Seleção IF-ELSE/exercicio05.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
+int somaDosDoisMaiores(int a, int b, int c) {
+    if (a >= b && a >= c) {
+        if (b >= c) {
+            return a + b;
+        } else {
+            return a + c;
+        }
+    } else if (b >= a && b >= c) {
+        if (a >= c) {
+            return b + a;
+        } else {
+            return b + c;
+        }
+    } else {
+        if (a >= b) {
+            return c + a;
+        } else {
+            return c + b;
+        }
+    }
+}
+
 int main()
 {
-    int ;
+    int num1, num2, num3;
+    int soma;
+    printf("Informe três números: \n");
+    scanf("%d%d%d", &num1, &num2, &num3);
+
+    soma = somaDosDoisMaiores(num1, num2, num3);
     
+    printf("%d", soma);
+
     return 0;
 }
